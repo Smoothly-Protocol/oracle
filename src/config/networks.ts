6e28e5ee7098c51115825025636df2dcede15b68
Kody Sale <106842341+ksale001@users.noreply.github.com>
2023-09-17 20:02:43 -0700

    Added Sassal's peerID

--- src/config/networks.ts
+++ src/config/networks.ts
@@ -5,7 +5,8 @@ export const Peers = [
   "16Uiu2HAkvyHWXj9caLxEPc2JbTWmapXqHpimTP4oqYZvSSoCAJtJ",
   "16Uiu2HAmR7AXaAmkikrGo4W6iYJSNseC1PEdMJ3LEyw4cU57283c",
   "16Uiu2HAmTAKstCuxMozUFcmXoTJHpUrcM3YJxV4uNVntYBPNBZjo",
-  "16Uiu2HAmAm1SD5s9tJpFwxj5uTEiSpbYG1QqCqFRH49q3xWFS1H6"
+  "16Uiu2HAmAm1SD5s9tJpFwxj5uTEiSpbYG1QqCqFRH49q3xWFS1H6",
+  "16Uiu2HAkvU861aiRsknGdppTv5PVqLjui4RYCGopMCn7corbJ75R"
 ];
 
 export const GOERLI: NetInfo = {
