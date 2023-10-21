8078c96d0787eb8b0684250af6d7f05a0848441a
noah figueras <noahfigueras@gmail.com>
2023-10-21 15:05:51 +0200

    fix: oracle.getBalance wasn't retrieving latest blockTag correctly in /poolstats api

--- src/config/config.ts
+++ src/config/config.ts
@@ -71,7 +71,7 @@ export class Config {
   async getBalance(blockNumber: string): Promise<BigNumber> {
     return await this.contract.provider.getBalance(
       this.contract.address, 
-      Number(blockNumber)
+      blockNumber === "latest" ? blockNumber : Number(blockNumber)
     )
   }
 
